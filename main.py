df3b5d8f5dc9b8568d376a90e3bb95d328db3b26
prof-deets <mark.dietrich@gmail.com>
2024-09-21 15:30:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print('hello comsc-210')
 print('this is the second commit')
 print('this is the third commit')
+
 print('this is still third')
+
 print('this is the fourth')
+
+print('this is the fifth')
